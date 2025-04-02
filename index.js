13e8d27fdb3a5d5d2894a0300c49e356d05353e8
BrendoFDev <brendo.dkry@gmail.com>
2025-04-02 11:03:36 -0300

    feat: criar evento de escolha do pedra papel e tesoura e timer de 5 seg.

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-const bttPlay = document.getElementById('bttPlay');
-const secTimer = document.getElementById('secTimer');
-let timerId;
-bttPlay.addEventListener("click",resetGameAndStart);
-
-function resetGameAndStart(){
-    document.getElementById("point").style.background = "none";
-    infinitTimer();
-
-}
-
-function infinitTimer(){
-    secTimer.innerText = 5;
-    timerId = setInterval(() => {
-        decrementValue();
-    }, 1000);
-}
-
-function decrementValue(){
-    if(secTimer.innerText == 0){
-        clearInterval(timerId);
-        return;
-    }
-    secTimer.innerText -= 1;
-}
-
-
 class Game {
     constructor() {
         this.enemyChoice = null;
         this.UserChoice = null;
+        this.timerId;
+
+        this.bttPlay = document.getElementById('bttPlay');
+        this.secTimer = document.getElementById('secTimer');
+        this.points = document.querySelectorAll('.point');
+        
+        bttPlay.addEventListener("click", this.resetGameAndStart.bind(this));
+
+        document.querySelectorAll('.buttons').forEach( button => {
+            button.addEventListener("click", (e) => this.getUserChoice(e));
+        });
     }
 
     getEnemyChoice() {
@@ -36,51 +20,83 @@ class Game {
 
         switch (randomValue) {
             case 1:
-                this.enemyChoice = "Rock";
-                break;
+                this.enemyChoice = "Rock"; break;
             case 2:
-                this.enemyChoice = "Paper";
-                break;
+                this.enemyChoice = "Paper"; break;
             case 3:
-                this.enemyChoice = "Scissor";
-                break;
+                this.enemyChoice = "Scissor"; break;
         }
+    }
+
+    getUserChoice(e){
+        this.UserChoice = e.target.dataset.choice;
+        this.getEnemyChoice();
+        this.CompareChoices();
     }
 
     CompareChoices() {
         if (this.enemyChoice == this.UserChoice) {
-            // Considerar Empate
+            this.draw();
         }
         else if (this.enemyChoice == "Rock") {
             if (this.UserChoice == "Scissor") {
-                // inimigo ganha
+                this.enemyPoint();
             }
             else {
-                // usuario ganha
+                this.userPoint();
             }
         }
         else if (this.enemyChoice == "Paper") {
             if (this.UserChoice == "Rock") {
-                // inimigo ganha
+                this.enemyPoint();
             }
             else {
-                // usuário ganha
+                this.userPoint();
             }
         }
         else if (this.enemyChoice == "Scissor") {
             if (this.UserChoice == "Paper") {
-                // inimigo ganha
+                this.enemyPoint();
             }
             else {
-                // usuário ganha
+                this.userPoint();
             }
         }
     }
 
-    getUserChoice(e) {
-
+    draw(){
+        console.log("draw")
     }
+
+    enemyPoint(){
+        console.log("enemypoint")
+    }
+
+    userPoint(){
+        console.log("userpoint")
+    }
+
+    resetGameAndStart(){
+        this.infinitTimer();
+    }
+
+    infinitTimer(){
+        secTimer.innerText = 5;
+        this.timerId = setInterval(() => {
+            this.decrementValue();
+        }, 1000);
+    }
+
+    decrementValue(){
+
+        if(secTimer.innerText == 0){
+            clearInterval(this.timerId);
+            return;
+        }
+
+        secTimer.innerText -= 1;
+    }
+
 }
 
 const game = new Game();
-game.getEnemyChoice();

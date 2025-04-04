34ed3d245411fba6579155d81cf5858e1a3477fc
BrendoFDev <brendo.dkry@gmail.com>
2025-04-04 11:04:09 -0300

    feat: Adicionar sistema de pontuação e corrigir timer da jogado e intevalo entre rodadas

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
 class Game {
     constructor() {
-        this.enemyChoice = null;
-        this.UserChoice = null;
+        this.GameStarted = false;
+        this.enemyChoice;
+        this.UserChoice;
         this.timerId;
-
+        this.rounds = 1;
+        this.useSpanPoint = document.querySelector("#userPoint span#point");
+        this.enemySpanPoint = document.querySelector("#enemyPoint span#point");
+        
+        
         this.bttPlay = document.getElementById('bttPlay');
         this.secTimer = document.getElementById('secTimer');
         this.points = document.querySelectorAll('.point');
-        
+
         bttPlay.addEventListener("click", this.resetGameAndStart.bind(this));
 
-        document.querySelectorAll('.buttons').forEach( button => {
+        document.querySelectorAll('.buttons').forEach(button => {
             button.addEventListener("click", (e) => this.getUserChoice(e));
         });
     }
 
-    getEnemyChoice() {
-        const randomValue = Math.floor(Math.random() * 3) + 1;
 
-        switch (randomValue) {
-            case 1:
-                this.enemyChoice = "Rock"; break;
-            case 2:
-                this.enemyChoice = "Paper"; break;
-            case 3:
-                this.enemyChoice = "Scissor"; break;
-        }
-    }
-
-    getUserChoice(e){
+    getUserChoice(e) {
         this.UserChoice = e.target.dataset.choice;
+        this.setUserChoiceColor();
         this.getEnemyChoice();
         this.CompareChoices();
+    }
+
+    getEnemyChoice() {
+        const choices = ["enemyRock","enemyPaper", "enemyScissor"];
+        
+        const randomValue = Math.floor(Math.random() * 2);
+        this.enemyChoice = choices[randomValue];
+        console.log(randomValue);
+    }
+
+    setUserChoiceColor(){
+        console.log(this.UserChoice)
+        const bttUser = document.querySelector(`#${this.UserChoice}`);
+
+        bttUser.style.border = "2px solid green";
+        bttUser.style.boxShadow = "1px 1px 10px green";
+    }
+
+    setEnemyChoiceColor() {
+        console.log(this.enemyChoice);
+        const bttEnemy = document.querySelector(`#${this.enemyChoice}`);
+
+        bttEnemy.style.border = "2px solid red";
+        bttEnemy.style.boxShadow = "1px 1px 10px red";
     }
 
     CompareChoices() {
         if (this.enemyChoice == this.UserChoice) {
             this.draw();
         }
-        else if (this.enemyChoice == "Rock") {
+        else if (this.enemyChoice == "enemyRock") {
             if (this.UserChoice == "Scissor") {
                 this.enemyPoint();
             }
@@ -46,7 +64,7 @@ class Game {
                 this.userPoint();
             }
         }
-        else if (this.enemyChoice == "Paper") {
+        else if (this.enemyChoice == "enemyPaper") {
             if (this.UserChoice == "Rock") {
                 this.enemyPoint();
             }
@@ -54,7 +72,7 @@ class Game {
                 this.userPoint();
             }
         }
-        else if (this.enemyChoice == "Scissor") {
+        else if (this.enemyChoice == "enemyScissor") {
             if (this.UserChoice == "Paper") {
                 this.enemyPoint();
             }
@@ -64,39 +82,70 @@ class Game {
         }
     }
 
-    draw(){
-        console.log("draw")
+    draw() {
+        this.userSpanPoint.innerText +=1;
+        this.enemySpanPoint.innerText +=1;
     }
 
-    enemyPoint(){
-        console.log("enemypoint")
+    enemyPoint() {
+        this.enemySpanPoint.innerText +=1;
     }
 
-    userPoint(){
-        console.log("userpoint")
+    userPoint() {
+        this.userSpanPoint.innerText +=1;
     }
 
-    resetGameAndStart(){
+    resetGameAndStart() {
         this.infinitTimer();
     }
 
-    infinitTimer(){
-        secTimer.innerText = 5;
+    infinitTimer() {
+        this.secTimer.innerText = 5;
         this.timerId = setInterval(() => {
             this.decrementValue();
         }, 1000);
     }
 
-    decrementValue(){
-
-        if(secTimer.innerText == 0){
+    decrementValue() {
+        secTimer.innerText -= 1;
+        if (this.secTimer.innerText == 0 && this.rounds != 3) {
             clearInterval(this.timerId);
+            this.setEnemyChoiceColor();
+            this.interval();
+            
+        }
+        else if (this.secTimer.innerText == 0 && this.rounds == 3) {
+            this.rounds = 1;
+            clearInterval(this.timerId);
+            this.resetGameAndStart();
             return;
         }
-
-        secTimer.innerText -= 1;
     }
 
+    interval(){
+        setTimeout(()=>{
+            this.resetEnemyChoice();
+            this.resetUserChoice();
+            this.infinitTimer();
+        },3000);
+    }
+
+    resetEnemyChoice() {
+        const bttEnemy = document.querySelector(`#${this.enemyChoice}`);
+        if (!bttEnemy) return;
+
+        bttEnemy.style.border = "2px solid white";
+        bttEnemy.style.boxShadow = "none";
+    }
+
+    resetUserChoice(){
+        const bttUser = document.querySelector(`#${this.UserChoice}`);
+        if (!bttUser) return;
+
+        bttUser.style.border = "2px solid white";
+        bttUser.style.boxShadow = "none";
+    }
+    
 }
 
 const game = new Game();
